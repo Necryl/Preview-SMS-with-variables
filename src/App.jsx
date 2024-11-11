38da9d80dc58cd8dccf4077ccb410d1ebdab228f
Necryl <74096664+Necryl@users.noreply.github.com>
2024-11-11 08:32:23 +0000

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,56 @@
 import { useState, useRef, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import Instance from "./Instance.jsx";
 import "./App.css";
 
 function App() {
   const [input, setInput] = useState("");
   const [variableCount, setVariableCount] = useState(0);
-  const [instanceCount, setInstanceCount] = useState(1);
   const [variableValues, setVariableValues] = useState({});
   const instanceCounter = useRef(0);
 
+  if (Object.keys(variableValues).length === 0 && variableCount > 0) {
+    createNewInstance();
+  }
+  function deleteInstance(id) {
+    let result = { ...variableValues };
+    delete result[id];
+    setVariableValues(result);
+  }
+
   function handleSMSinput(e) {
     setInput(e.target.value);
+  }
+
+  function onVarChange(id, vars) {
+    setVariableValues((prevState) => {
+      const result = {
+        ...prevState,
+      };
+
+      result[id].data = vars;
+
+      return result;
+    });
+  }
+
+  function createNewInstance() {
+    const newInstance = generateInstance(variableCount);
+    const result = { ...variableValues };
+    result[newInstance.id] = newInstance;
+    setVariableValues(result);
   }
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
       const matches = (input.match(/{#var#}/g) || []).length;
       setVariableCount(matches);
-      console.log(Object.keys(variableValues).length);
-      if (Object.keys(variableValues).length === 0 && variableCount > 0) {
-        console.log("CReating new instance");
-        const newInstance = generateInstance(variableCount);
-        const result = {};
-        result[newInstance.id] = newInstance;
-        setVariableValues(result);
-      }
     }, 300); // Adjust delay as needed (300ms here)
 
     return () => clearTimeout(timeoutId); // Clear timeout if input changes within delay
   }, [input]);
 
   function generateInstance(varCount, data = []) {
-    const id = "Instance" + instanceCounter;
+    const id = "Instance" + instanceCounter.current;
     instanceCounter.current++;
     return {
       id: id,
@@ -41,57 +58,69 @@ function App() {
     };
   }
 
-  function handleVariableChange(value, instanceID, varIndex) {
-    const result = JSON.parse(JSON.stringify(variableValues));
-    result[instanceID].data[varIndex] = value;
-    setVariableValues(result);
-  }
-
   return (
-    <>
+    <div id="App">
       <div id="input">
         <textarea
           id="inputElem"
           onChange={handleSMSinput}
           value={input}
+          placeholder="Paste SMS here with variables as {#var#}"
         ></textarea>
       </div>
       <div id="instances">
         <div id="instanceMenu">
-          <button>Add</button>
+          <button
+            onClick={() => {
+              createNewInstance();
+            }}
+          >
+            Add
+          </button>
         </div>
         <div id="board">
-          {Object.keys(variableValues).map((instance) => (
+          {Object.keys(variableValues).map((instance, i) => (
             <Instance
               data={variableValues[instance].data}
-              index={variableValues[instance].id}
+              name={variableValues[instance].id}
+              index={i}
               key={variableValues[instance].id}
+              onVarChange={(vars) => {
+                onVarChange(variableValues[instance].id, vars);
+              }}
+              deleteInstance={() => {
+                deleteInstance(variableValues[instance].id);
+              }}
             />
           ))}
         </div>
       </div>
       <div id="result">
-        <p>
-          {(() => {
-            function replacePlaceholders(text, values) {
-              let index = 0; // To keep track of the array index
+        {(() => {
+          function replacePlaceholders(text, values) {
+            let index = 0; // To keep track of the array index
 
-              // Replace each "{#var#}" with the next value in the array, if available
-              return text.replace(/{#var#}/g, () => values[index++] || "");
-            }
-            let result = "";
-            Object.keys(variableValues).forEach((instance) => {
-              let text = replacePlaceholders(
-                input,
-                variableValues[instance].data
-              );
-              result += text + "\n\n";
-            });
-            return result;
-          })()}
-        </p>
+            // Replace each "{#var#}" with the next value in the array, if available
+            return text.replace(/{#var#}/g, () => values[index++] || "");
+          }
+          return Object.keys(variableValues).map((instance) => {
+            let text = replacePlaceholders(
+              input,
+              variableValues[instance].data
+            );
+            return (
+              <p>
+                {text.split("\n").map((text) => (
+                  <>
+                    {text} <br />
+                  </>
+                ))}
+              </p>
+            );
+          });
+        })()}
       </div>
-    </>
+    </div>
   );
 }
 
